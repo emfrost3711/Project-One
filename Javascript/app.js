7381d13684f33037cc3a217fd1f8eadb395e03ba
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-06-02 15:51:16 -0600

    logic for recipe button, array for recipes

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,20 +1,40 @@
-//Link to Edamam API
- src="https://code.jquery.com/jquery-3.4.1.min.js"
- integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
- crossorigin="anonymous">
+
+//need to add button id's to HTML
+ 
+ var inputRecipe = ["tomato", "chicken", "green beans", "keto", "vegan"]
 
         console.log("working")
-        var searchEdamam = function(recipe) {
-     var queryURL = "https://api.edamam.com/search?q=italian&app_id=997d5b1e&app_key=742f4d94c84ebe56fad332789e47f512";
+        var searchEdamam = function(chili) {
+          //Link to Edamam API
+     var queryURL = "https://api.edamam.com/search?q=" + inputRecipe + "&app_id=997d5b1e&app_key=742f4d94c84ebe56fad332789e47f512";
 
      $.ajax({
        url: queryURL,
        method: "GET"
-     }).then(function(response) {
-       console.log(response)
+     }).then(function(chili) {
+       console.log(chili)
      });
      
    };
+   $(".recipe-search").keyup(function(){
+    var userRecipe = $("#recipe-input").val().trim();
+    inputRecipe.push(userRecipe)
+    //generate random recipe using keyword
+   })
+   $("#recipeButton").on("click", function(){
+     if(1<2
 
-searchOMDB();
+      //  .recipe-search input does not exist
+      ){
+      //generate random recipe from full website
+      //maybe pulling keyword from an array of random keywords we provide
+     }
+     else{
+      //generate new random recipe using keyword user provided
+     }
+    //generate random recipe using key word, pulled from edamam
+   })
+
+   //need to connect
+   searchEdamam();
         
